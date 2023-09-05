41b5641902fe1138cc1abd9aa86508d646441304
mikcbn <136186783+mikcbn@users.noreply.github.com>
2023-09-06 01:41:03 +0200

    .

--- scripts.js
+++ scripts.js
@@ -1,4 +1,50 @@
-import { TABLES, COLUMNS, state, createOrderData, updateDragging, html, createOrderHtml, moveToColumn, updateDraggingHtml } from './view.js';
+import { TABLES, COLUMNS, state, createOrderData, updateDragging, createOrderHtml, moveToColumn, updateDraggingHtml } from './view.js';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const html = {
+        add: {
+            overlay: document.querySelector('[data-add-overlay]'),
+            title: document.querySelector('[data-add-title]'),
+            table: document.querySelector('[data-add-table]'),
+            cancel: document.querySelector('[data-add-cancel]'),
+            form: document.querySelector('[data-add-form]')
+        },
+        edit: {
+            overlay: document.querySelector('[data-edit-overlay]'),
+            id: document.querySelector('[data-edit-id]'),
+            title: document.querySelector('[data-edit-title]'),
+            table: document.querySelector('[data-edit-table]'),
+            column: document.querySelector('[data-edit-column]'),
+            cancel: document.querySelector('[data-edit-cancel]'),
+            form: document.querySelector('[data-edit-form]'),
+            delete: document.querySelector('[data-edit-delete]')
+        },
+        help: {
+            overlay: document.querySelector('[data-help-overlay]'),
+            cancel: document.querySelector('[data-help-cancel]')
+        },
+        other: {
+            add: document.querySelector('[data-add]'),
+            grid: document.querySelector('[data-grid]')
+        },
+        columns: {
+            ordered: document.querySelector('[data-column="ordered"]'),
+            preparing: document.querySelector('[data-column="preparing"]'),
+            served: document.querySelector('[data-column="served"]')
+        },
+        area: {
+            ordered: document.querySelector('[data-area="ordered"]'),
+            preparing: document.querySelector('[data-area="preparing"]'),
+            served: document.querySelector('[data-area="served"]')
+        }
+    };
+
+    // Function to set focus on the "Add Order" button
+    const setFocusOnAddButton = () => {
+        html.other.add.focus();
+    };
+
 
 /**
  * Event handler for opening the "Add Order" overlay.
@@ -8,7 +54,6 @@ const handleAddToggle = () => {
     html.add.title.value = '';
     html.add.table.selectedIndex = 0;
 };
-
 /**
  * Event handler for submitting the "Add Order" form.
  */
@@ -28,7 +73,6 @@ const handleAddSubmit = (event) => {
         html.add.overlay.close();
     }
 };
-
 /**
  * Event handler for opening the "Edit Order" overlay.
  */
@@ -97,142 +141,142 @@ const handleDelete = (event) => {
         html.edit.overlay.close();
     }
 };
+
 /**
  * Event handler for opening the "Help" overlay.
  */
-const handleHelpToggle = () => {
-    const helpOverlay = html.help.overlay;
+    const handleHelpToggle = () => {
+        const helpOverlay = html.help.overlay;
 
-    if (helpOverlay.open) {
-        helpOverlay.close();
-        html.other.add.focus(); // Return focus to the "Add Order" button
-    } else {
-        const helpContent = document.querySelector('[data-help-overlay]'); // Get the Help overlay content
-        const helpTitle = helpContent.querySelector('.overlay__title').textContent; // Get the title
-        const helpText = Array.from(helpContent.querySelectorAll('p')).map((p) => p.textContent).join('\n'); // Get the paragraphs
+        if (helpOverlay.open) {
+            helpOverlay.close();
+            setFocusOnAddButton(); // Focus back to "Add Order" button
+        } else {
+            const helpContent = document.querySelector('[data-help-overlay]');
+            const helpTitle = helpContent.querySelector('.overlay__title').textContent;
+            const helpText = Array.from(helpContent.querySelectorAll('p')).map((p) => p.textContent).join('\n');
 
-        const helpMessage = `${helpTitle}\n\n${helpText}`; // Combine the title and paragraphs
+            const helpMessage = `${helpTitle}\n\n${helpText}`;
 
-        // Display the help message in an alert (you can replace this with your desired popup method)
-        alert(helpMessage);
-
-        // If you want to show the overlay instead of an alert, you can use the following line:
-        // helpOverlay.showModal();
-    }
-};
-
-html.help.addEventListener('click', handleHelpToggle);
-
-
-const addOverlay = document.createElement('dialog');
-addOverlay.setAttribute('data-add-overlay', ''); 
-/**
- * Event handler for closing an overlay (Add Order, Edit Order, or Help).
- */
-const handleCloseOverlay = (event) => {
-    const overlay = event.target.closest('dialog');
-    if (overlay) {
-        overlay.close();
-        html.add.overlay.close();
-        html.edit.overlay.close();
-        html.help.overlay.close();
-        html.add.overlay.remove();
-        html.edit.overlay.remove();
-        html.help.overlay.remove();
-        html.add.overlay.show();
-    }
-
-    html.other.add.focus();
-};
-
-
-html.add.cancel.addEventListener('click', handleCloseOverlay);
-html.edit.cancel.addEventListener('click', handleCloseOverlay);
-html.help.cancel.addEventListener('click', handleCloseOverlay);
-
-/**
- * Event handler for drag start.
- */
-const handleDragStart = (event) => {
-    const orderId = event.target.dataset.id;
-    if (!orderId) return;
-
-    event.dataTransfer.setData('text/plain', orderId);
-    event.dataTransfer.effectAllowed = 'move';
-    updateDragging({ source: orderId });
-};
-
-/**
- * Event handler for drag end.
- */
-const handleDragEnd = () => {
-    updateDragging({ source: null });
-    updateDraggingHtml({});
-};
-
-/**
- * Event handler for drag over.
- */
-const handleDragOver = (event) => {
-    event.preventDefault();
-    const path = event.path || event.composedPath();
-    let column = null;
-
-    for (const element of path) {
-        const { area } = element.dataset;
-        if (area) {
-            column = area;
-            break;
+            alert(helpMessage);
         }
+    };
+    // Event delegation for "Help" and "Add Order" buttons
+    document.body.addEventListener('click', (event) => {
+        if (event.target.id === 'help-button') {
+            handleHelpToggle();
+        } else if (event.target.id === 'add-button') {
+            handleAddToggle();
+        }
+    });
+
+    const addOverlay = document.createElement('dialog');
+    addOverlay.setAttribute('data-add-overlay', '');
+
+    /**
+     * Event handler for closing an overlay (Add Order, Edit Order, or Help).
+     */
+    const handleCloseOverlay = (event) => {
+        const overlay = event.target.closest('dialog');
+        if (overlay) {
+            overlay.close();
+            html.add.overlay.close();
+            html.edit.overlay.close();
+            html.help.overlay.close();
+            html.add.overlay.remove();
+            html.edit.overlay.remove();
+            html.help.overlay.remove();
+            setFocusOnAddButton(); // Focus back to "Add Order" button
+        }
+    };
+    
+    // Initialize event listeners.
+    html.other.add.addEventListener('click', handleAddToggle);
+    html.add.cancel.addEventListener('click', handleCloseOverlay);
+    html.edit.cancel.addEventListener('click', handleCloseOverlay);
+    html.help.cancel.addEventListener('click', handleCloseOverlay);
+    
+    /**
+     * Event handler for drag start.
+     */
+    const handleDragStart = (event) => {
+        const orderId = event.target.dataset.id;
+        if (!orderId) return;
+
+        event.dataTransfer.setData('text/plain', orderId);
+        event.dataTransfer.effectAllowed = 'move';
+        updateDragging({ source: orderId });
+    };
+
+    /**
+     * Event handler for drag end.
+     */
+    const handleDragEnd = () => {
+        updateDragging({ source: null });
+        updateDraggingHtml({});
+    };
+
+    /**
+     * Event handler for drag over.
+     */
+    const handleDragOver = (event) => {
+        event.preventDefault();
+        const path = event.path || event.composedPath();
+        let column = null;
+
+        for (const element of path) {
+            const { area } = element.dataset;
+            if (area) {
+                column = area;
+                break;
+            }
+        }
+
+        if (!column) return;
+        updateDragging({ over: column });
+        updateDraggingHtml({ over: column });
+    };
+
+        /**
+     * Initialize event listeners.
+     */
+    html.add.form.addEventListener('submit', handleAddSubmit);
+    html.other.add.addEventListener('click', handleAddToggle);
+    html.other.grid.addEventListener('click', handleEditToggle);
+    html.edit.overlay.addEventListener('close', handleCloseOverlay);
+    html.edit.cancel.addEventListener('click', handleEditToggle);
+    html.edit.form.addEventListener('submit', handleEditSubmit);
+    html.edit.delete.addEventListener('click', handleDelete);
+  
+    html.other.add.addEventListener('click', handleAddToggle);
+    html.add.cancel.addEventListener('click', handleCloseOverlay);
+    html.edit.cancel.addEventListener('click', handleCloseOverlay);
+    html.help.cancel.addEventListener('click', handleCloseOverlay);
+    
+
+    for (const htmlColumn of Object.values(html.columns)) {
+        htmlColumn.addEventListener('dragstart', handleDragStart);
+        htmlColumn.addEventListener('dragend', handleDragEnd);
     }
 
-    if (!column) return;
-    updateDragging({ over: column });
-    updateDraggingHtml({ over: column });
-};
-
-/**
- * Initialize event listeners.
- */
-html.add.overlay.addEventListener('close', handleCloseOverlay);
-html.add.cancel.addEventListener('click', handleAddToggle);
-html.add.form.addEventListener('submit', handleAddSubmit);
-html.other.add.addEventListener('click', handleAddToggle);
-html.other.grid.addEventListener('click', handleEditToggle);
-html.edit.overlay.addEventListener('close', handleCloseOverlay);
-html.edit.cancel.addEventListener('click', handleEditToggle);
-html.edit.form.addEventListener('submit', handleEditSubmit);
-html.edit.delete.addEventListener('click', handleDelete);
-html.help.overlay.addEventListener('close', handleCloseOverlay);
-html.help.cancel.addEventListener('click', handleHelpToggle);
-
-for (const htmlColumn of Object.values(html.columns)) {
-    htmlColumn.addEventListener('dragstart', handleDragStart);
-    htmlColumn.addEventListener('dragend', handleDragEnd);
-}
-
-for (const htmlArea of Object.values(html.area)) {
-    htmlArea.addEventListener('dragover', handleDragOver);
-}
-
-html.add.table.appendChild(createTableOptionsHtml());
-html.edit.table.appendChild(createTableOptionsHtml());
-
-/**
- * Start the app focused on the "Add Order" button.
- */
-html.other.add.focus();
-
-/**
- * Helper function to create table options in the "Add Order" and "Edit Order" forms.
- */
-function createTableOptionsHtml() {
-    const fragment = document.createDocumentFragment();
-    for (const singleTable of TABLES) {
-        const option = document.createElement('option');
-        option.value = singleTable;
-        option.innerText = singleTable;
-        fragment.appendChild(option);
+    for (const htmlArea of Object.values(html.area)) {
+        htmlArea.addEventListener('dragover', handleDragOver);
     }
-    return fragment;
-}
+
+    html.add.table.appendChild(createTableOptionsHtml());
+    html.edit.table.appendChild(createTableOptionsHtml());
+
+    /**
+     * Helper function to create table options in the "Add Order" and "Edit Order" forms.
+     */
+    function createTableOptionsHtml() {
+        const fragment = document.createDocumentFragment();
+        for (const singleTable of TABLES) {
+            const option = document.createElement('option');
+            option.value = singleTable;
+            option.innerText = singleTable;
+            fragment.appendChild(option);
+        }
+        return fragment;
+    }
+});
